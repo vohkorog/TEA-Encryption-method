85c798c22b1a4a2ff8d1b736d2d7388ce153faf0
kirill <kigo01@yandex.ru>
2025-09-26 17:26:37 +0300

    create Window application

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("start project")
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QLineEdit, QVBoxLayout, QPushButton
+
+
+class Window(QWidget):
+    
+    
+    def __init__(self):
+        super().__init__()
+        self.__initField()
+
+    def __initField(self):
+        self.setWindowTitle("Window")
+        self.resize(1000,500)
+
+    
+app = QApplication(sys.argv)
+window = Window()
+window.show()
+sys.exit(app.exec_())
+    
